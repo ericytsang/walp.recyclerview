be80df11002808b35cbba478eb2ee22eb3599d8c
Eric Tsang <surplus.et@gmail.com>
2015-10-12 12:14:43 -0700

    more comments...
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- app/src/main/java/gmail/surpluset/recyclerviewanimations/Adapter.java
+++ app/src/main/java/gmail/surpluset/recyclerviewanimations/Adapter.java
@@ -9,9 +9,25 @@ import java.util.List;
 
 public class Adapter extends RecyclerView.Adapter<StringViewHolder>
 {
+    /**
+     * long string to generate gibberish from for the list items.
+     */
     private static final String LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    /**
+     * reference to this adapter's listener. the listener's methods are invoked
+     *   when events occur relevant to this adapter.
+     */
     private Listener adapterListener;
+
+    /**
+     * reference to the adapter's underlying data source.
+     */
     private final List<String> items;
+
+    /**
+     * listener to pass to viewholders managed by this adapter.
+     */
     private final StringViewHolderListener stringViewHolderListener;
 
     // public interface: constructors
@@ -22,6 +38,10 @@ public class Adapter extends RecyclerView.Adapter<StringViewHolder>
         this.items = new ArrayList<>();
         this.stringViewHolderListener = new StringViewHolderListener();
 
+        // important: the following line of code lets listening views know that
+        // they can depend on the values provided by the getItemId method for
+        // different list items, so they may implement animations of an item
+        // moving from one position to another by looking at their id
         setHasStableIds(true);
     }
 
@@ -45,12 +65,29 @@ public class Adapter extends RecyclerView.Adapter<StringViewHolder>
         holder.setModel(items.get(position));
     }
 
+    /**
+     * should return a unique number for each element in the adapter's data
+     *   source.
+     *
+     * @param position position of the element in the data source to get the ID
+     *   of.
+     *
+     * @return id of the element at the specified position.
+     */
     @Override
     public long getItemId(int position)
     {
-        return items.get(position).hashCode(); // can be improved because 2 different strings could have the same hashcode...
+        // IMPORTANT: the code below can be improved because 2 different strings
+        // could have the same hashcode...although the recyclerview won't
+        // exception if multiple elements return the same IDs.
+        return items.get(position).hashCode();
     }
 
+    /**
+     * returns the number of elements in the adapter's underlying data source.
+     *
+     * @return the number of elements in the adapter's underlying data source.
+     */
     @Override
     public int getItemCount()
     {
